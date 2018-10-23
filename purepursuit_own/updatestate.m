ac1b0266a84b23a6bcd9d57d5ec1be723a711e0d
Sharon Babu <ubsharon93@gmail.com>
2018-10-23 23:59:15 +0200

    algo update

--- purepursuit_own/updatestate.m
+++ purepursuit_own/updatestate.m
@@ -1,7 +1,12 @@
-function state = updatestate(state, delta):
-
-    state.x = state.x + state.v * cos(requiredheading) * dt
-    state.y = state.y + state.v * sin(requiredheading) * dt
-    state.yaw = state.yaw + state.v / L * tan(steeringangle) * dt
-    state.v = state.v + a * dt
+function [statex, statey] = updatestate(X, Y, delta)
+    statex = X;
+    statey = Y;
+    statev = 10;
+    stateyaw = 0;
+    L=2.9;
+    dt = 0.1;
+    statex = statex + statev * cos(delta) * dt
+    statey = statey + statev * sin(delta) * dt
+    stateyaw = stateyaw + statev / L * tan(delta) * dt
+    dt = dt+1;
 end
